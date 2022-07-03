00ed8cd3565c53675d4810b207d10193bbcb5dac
sohaibsalman13 <86664458+sohaibsalman13@users.noreply.github.com>
2022-07-03 22:10:51 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup
 import requests
 import re
+from csv import writer
 
 product = input("What product you want to search?")
 
@@ -16,27 +17,32 @@ pages = int(str(page_text).split("/")[-2].split(">")[-1][:-1])
 items_found = {}
 
 # iterating through pages
-for page in range(1, pages+1):
-    url = f"https://www.newegg.com/p/pl?d={product}&N=8000%204131&page={page}"
-    page = requests.get(url).text
-    doc = BeautifulSoup(page, "html.parser")
+with open('products.csv', 'w', encoding='utf8', newline='') as f:
 
-    # searching just the products under the tag
-    div = doc.find(class_="item-cells-wrap border-cells items-grid-view four-cells expulsion-one-cell")
+    thewriter = writer(f)
+    header = ['Price', 'Link']
+    thewriter.writerow(header)
+    for page in range(1, pages+1):
+        url = f"https://www.newegg.com/p/pl?d={product}&N=8000%204131&page={page}"
+        page = requests.get(url).text
+        doc = BeautifulSoup(page, "html.parser")
 
-    # only searching the item input by the user
-    items = div.find_all(text=re.compile(product))
+        # searching just the products under the tag
+        div = doc.find(class_="item-cells-wrap border-cells items-grid-view four-cells expulsion-one-cell")
 
-    for item in items:
-        parent = item.parent
-        if parent.name != "a":
-            continue
+        # only searching the item input by the user
+        items = div.find_all(text=re.compile(product))
 
-        link = parent['href']
-        next_parent = item.find_parent(class_="item-container")
-        price = next_parent.find(class_="price-current").strong.string
+        for item in items:
+            parent = item.parent
+            if parent.name != "a":
+                continue
 
-        items_found[item] = {"price": int(price.replace(",", "")), "link": link}
+            link = parent['href']
+            next_parent = item.find_parent(class_="item-container")
+            price = next_parent.find(class_="price-current").strong.string
+
+            items_found[item] = {"price": int(price.replace(",", "")), "link": link}
 
 
-print(items_found)
+    thewriter.writerow(items_found)
